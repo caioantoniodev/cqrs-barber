4497e4f40c8335fffe7db7951e4052bf59cad0d3
Caio Antonio <caio.antonio.c@outlook.com>
2024-08-18 17:20:39 -0300

    project init

---
+++ ms-barber-command/src/main/java/tech/dev/msbarbercommand/MsBarberCommandApplication.java
@@ -0,0 +1,13 @@
+package tech.dev.msbarbercommand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBarberCommandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsBarberCommandApplication.class, args);
+	}
+
+}
---
+++ ms-barber-command/src/test/java/tech/dev/msbarbercommand/MsBarberCommandApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.dev.msbarbercommand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsBarberCommandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
